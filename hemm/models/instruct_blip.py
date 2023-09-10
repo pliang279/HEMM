34d2c9dfb53926489765a41598c800f86c5a10dc
Akshay Goindani <asgoindani@gmail.com>
2023-09-10 15:18:52 -0400

    instruct blip through lavis

--- hemm/models/instruct_blip.py
+++ hemm/models/instruct_blip.py
@@ -4,7 +4,7 @@ from typing import Optional, Union
 from PIL import Image
 from tqdm import tqdm
 from hemm.models.model import HEMMModel
-from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration
+from lavis.models import load_model_and_preprocess
 
 class InstructBlip(HEMMModel):
 	def __init__(self,
@@ -15,30 +15,20 @@ class InstructBlip(HEMMModel):
 		self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 	def load_weights(self):
-		self.model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b").to(self.device)
-		self.processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b").to(self.device)
+		self.model, self.vis_processors, _ = load_model_and_preprocess(name="blip2_vicuna_instruct", model_type="vicuna7b", is_eval=True, device=self.device)
 
 	def generate(self,
 				text: Optional[str],
 				image,
 			) -> str:
-		image = Image.open(image).convert("RGB")
-		inputs = self.processor(images=image, text=text, return_tensors="pt").to(self.device)
-		outputs = self.model.generate(**inputs,
-								do_sample=False,
-								num_beams=5,
-								max_length=256,
-								min_length=1,
-								top_p=0.9,
-								repetition_penalty=1.5,
-								length_penalty=1.0,
-								temperature=1,
-								)
-		generated_text = self.processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-		return generated_text
+		raw_image = Image.open(image).convert("RGB")
+		image = self.vis_processors["eval"](raw_image).unsqueeze(0).to(self.device)
+		generated_text = self.model.generate({"image":image, "prompt":text})
+		return generated_text[0]
 		
 	def get_image_tensor(self, image):
-		img = self.processor.image_processor.preprocess(image, return_tensors="pt")["pixel_values"].to(self.device)
+		img = self.vis_processors["eval"](image).unsqueeze(0).to(self.device)
+		return img
 
 	def generate_batch(self, 
 					   images,
@@ -49,19 +39,7 @@ class InstructBlip(HEMMModel):
 		for i in tqdm(range(0, len(texts), batch_size)):
 			img_batch = images[i : i + batch_size]
 			text_batch = texts[i : i + batch_size]
-			text_inputs = self.processor(text=text_batch, return_tensors="pt", padding="max_length", max_length=256, truncation=True).to(self.device)
-			outputs = self.model.generate(**text_inputs,
-									pixel_values=img_batch,
-									do_sample=False,
-									num_beams=5,
-									max_length=256,
-									min_length=1,
-									top_p=0.9,
-									repetition_penalty=1.5,
-									length_penalty=1.0,
-									temperature=1)
-			
-			generated_text = self.processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
+			generated_text = self.model.generate({"image":img_batch, "prompt":text_batch})
 			answers += generated_text
-		
+				
 		return answers
