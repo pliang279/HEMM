444489beacb90186595a864d8ec13d6d72ef9e32
Akshay Goindani <asgoindani@gmail.com>
2023-09-18 17:29:21 -0400

    added the decimer dataset

---
+++ hemm/prompts/decimer_prompt.py
@@ -0,0 +1,7 @@
+class DecimerPrompt:
+	def __init__(self):
+		self.prompt = """ Simplified molecular-input line-entry system (SMILES) notation of the given molecule: """
+
+	def format_prompt(self, text=None):
+		return self.prompt
+	
