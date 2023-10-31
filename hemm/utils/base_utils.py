ee5df2c4590466c7db99a0e6cf9acde74532beb5
Akshay Goindani <asgoindani@gmail.com>
2023-10-31 14:35:05 -0400

    added updated base utils

--- hemm/utils/base_utils.py
+++ hemm/utils/base_utils.py
@@ -1,12 +1,14 @@
-import sys
-
-# sys.path.append("/home/agoindan/HEMM/hemm/metrics")
-# sys.path.append("/home/agoindan/HEMM/hemm/models")
-# sys.path.append("/home/agoindan/HEMM/hemm/data")
-
 from hemm.metrics import accuracy_metric, bleu_metric, bertscore_metric
 from hemm.models import blip2_model, minigpt4_model, instruct_blip, gill_model, kosmos2
-from hemm.data import newyorkercartoon_dataset, hateful_memes_dataset, nocaps_dataset, memotion_dataset, memecaps_dataset, irfl_dataset, scienceQA_dataset, vqa_dataset,vcr_dataset, ok_vqa_dataset, gqa_dataset, vqarad_dataset, pmcvqa_dataset, pathvqa_dataset, rsicd_dataset, ucmerced_dataset, resisc45_dataset,winogroundVQA_dataset,nlvr2,nlvr_dataset, faceemotion_dataset,cocoqa_dataset,visualgenome_dataset
+from hemm.data import newyorkercartoon_dataset, hateful_memes_dataset, nocaps_dataset, \
+                        memotion_dataset, memecaps_dataset, irfl_dataset, scienceQA_dataset, \
+                        vqa_dataset,vcr_dataset, ok_vqa_dataset, gqa_dataset, vqarad_dataset, \
+                        pmcvqa_dataset, pathvqa_dataset, rsicd_dataset, ucmerced_dataset, \
+                        resisc45_dataset, winogroundVQA_dataset, nlvr2, nlvr_dataset, faceemotion_dataset, \
+                        cocoqa_dataset, visualgenome_dataset, screen2words_dataset, flick30k_new_dataset, \
+                        decimer_dataset, enrico_dataset, inat_dataset, magic_brush_dataset, mmimdb_dataset, \
+                        plip_kather_dataset, slake_dataset
+                        
 
 def load_model(model_key):
     model_dict = {
@@ -27,7 +29,7 @@ def load_metric(metric_key):
     }
     return metric_dict[metric_key]
 
-def load_dataset_evaluator(dataset_key, kaggle_api_path=None):
+def load_dataset_evaluator(kaggle_api_path=None):
     dataset_dict = {
         'hateful_memes':hateful_memes_dataset.HatefulMemesDatasetEvaluator(
             kaggle_api_path=kaggle_api_path
@@ -38,12 +40,12 @@ def load_dataset_evaluator(dataset_key, kaggle_api_path=None):
             kaggle_api_path=kaggle_api_path
         ),
         'memecaps':memecaps_dataset.MemeCapsDatasetEvaluator(),
-        'irfl':irfl_dataset.IRFLDatasetEvaluator(),
+        # 'irfl':irfl_dataset.IRFLDatasetEvaluator(),
         'scienceqa':scienceQA_dataset.ScienceQADatasetEvaluator(),
         'vqa': vqa_dataset.VQADatasetEvaluator(),
-        'vcr': vcr_dataset.VCRDatasetEvaluator(),
+        # 'vcr': vcr_dataset.VCRDatasetEvaluator(),
         'okvqa': ok_vqa_dataset.OKVQADatasetEvaluator(dataset_dir='/work/agoindan/.cache/'),
-        'gqa': gqa_dataset.GQADatasetEvaluator(),
+        # 'gqa': gqa_dataset.GQADatasetEvaluator(),
         # 'winogroundVQA': winogroundVQA_dataset.WinogroundVQAEvaluator(),
         # 'nlvr':nlvr_dataset.NLVRDatasetEvaluator(),
         # 'nlvr2':nlvr2.NLVR2evaluator(),
@@ -54,14 +56,22 @@ def load_dataset_evaluator(dataset_key, kaggle_api_path=None):
         'ucmerced':ucmerced_dataset.UCMercedDatasetEvaluator(
             kaggle_api_path=kaggle_api_path
         ),
-        'resisc45':resisc45_dataset.Resisc45DatasetEvaluator(
-            kaggle_api_path=kaggle_api_path
-        ),
+        # 'resisc45':resisc45_dataset.Resisc45DatasetEvaluator(
+        #     kaggle_api_path=kaggle_api_path
+        # ),
         'face_emotion': faceemotion_dataset.FaceEmotionDatasetEvaluator(
             kaggle_api_path=kaggle_api_path
         ),
-        'screen2words':screen2words_dataset.Screen2WordsDatasetEvaluator(
-            kaggle_api_path=kaggle_api_path
-        )
+        # 'screen2words':screen2words_dataset.Screen2WordsDatasetEvaluator(
+        #     kaggle_api_path=kaggle_api_path
+        # ),
+        'decimer':decimer_dataset.DecimerDatasetEvaluator(),
+        'slake':slake_dataset.SlakeDatasetEvaluator(),
+        'enrico': enrico_dataset.EnricoDatasetEvaluator(),
+        'flickr30k': flick30k_new_dataset.Flickr30kDatasetEvaluator(),
+        'inat': inat_dataset.INATDatasetEvaluator(),
+        'magic_brush': magic_brush_dataset.MagicBrushDatasetEvaluator(),
+        'mmimdb': mmimdb_dataset.MMIMDBDatasetEvaluator(), 
     }
-    return dataset_dict[dataset_key]
+    # return dataset_dict[dataset_key]
+    return dataset_dict
